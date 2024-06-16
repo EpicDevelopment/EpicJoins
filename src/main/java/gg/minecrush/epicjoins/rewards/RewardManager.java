53f8c775b2ee77a58f46ace4731fab8ce7bbd35b
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-15 19:59:53 -0500

    update :)

--- src/main/java/gg/minecrush/epicjoins/rewards/RewardManager.java
+++ src/main/java/gg/minecrush/epicjoins/rewards/RewardManager.java
@@ -4,6 +4,7 @@ import gg.minecrush.epicjoins.EpicJoins;
 import gg.minecrush.epicjoins.storage.Config;
 import gg.minecrush.epicjoins.storage.Lang;
 import gg.minecrush.epicjoins.util.Color;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -27,8 +28,11 @@ public class RewardManager {
 
         if (material != null) {
             ItemStack reward = new ItemStack(material, amount);
+            Bukkit.getScheduler().runTask(plugin, () -> {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), plugin.getConfig().getString("reward.command").replace("%player%", player.getName()));
+            });
             player.getInventory().addItem(reward);
-            player.sendMessage(Color.c(lang.getReplacedMessage("reward-message")));
+            player.sendMessage(Color.c(lang.getReplacedMessage("reward-message").replace("%reward%", config.getValue("reward.name"))));
         } else {
             String prefix = config.getValue("prefix");
             player.sendMessage(Color.c(prefix + " &cInvalid reward item loaded in config.yml!"));
